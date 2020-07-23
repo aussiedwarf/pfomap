688e7954886a0bd23d03c25004eae597d00114ab
Eden Harris <aussiedwarf@gmail.com>
2020-07-23 21:48:46 +1000

    Updated copyright notice to 2020

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
-Copyright (C) 2019 Eden Harris
+Copyright (C) 2020 Eden Harris
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
